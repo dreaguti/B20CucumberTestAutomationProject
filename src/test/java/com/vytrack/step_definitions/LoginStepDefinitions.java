73c597f32561baa8a075254826a83021d09ffc99
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-10-20 21:39:11 -0400

    updated LoginPage.java class

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -5,15 +5,9 @@ import com.vytrack.utils.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefinitions {
-    WebDriver driver;
     LoginPage loginPage = new LoginPage();
 
     @Given("user is on the login page")
@@ -30,7 +24,7 @@ public class LoginStepDefinitions {
     @Then("user should see dashboard page")
     public void user_should_see_dashboard_page() {
         String expected = "Dashboard";
-        String actual = driver.findElement(By.className("oro-subtitle")).getText().trim();
+        String actual = loginPage.getPageSubTitleText();
 
         Assert.assertEquals("Title is not correct!", expected, actual);
         System.out.println("I see the Dashboard page!");
